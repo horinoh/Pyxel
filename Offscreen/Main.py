bcc543cdc1f94707fe391880ccc6c658c3942020
horinoh <development@horino.sakura.ne.jp>
2025-01-15 00:04:48 +0900

    コード更新

--- Offscreen/Main.py
+++ Offscreen/Main.py
@@ -20,22 +20,32 @@ class App:
         # バックバッファの準備
         self.Backbuffer = pyxel.Image(pyxel.width, pyxel.height)
         
+        self.IsRaster = False
+
         # 更新、描画関数を指定して実行
         pyxel.run(self.update, self.draw)
         
     # 更新関数
     def update(self):
-        pass
+        if pyxel.btnp(pyxel.GAMEPAD1_BUTTON_A) or pyxel.btnp(pyxel.KEY_A):
+            self.IsRaster = False if self.IsRaster else True
 
     # 描画関数
     def draw(self):
         # バックバッファクリア
-        self.Backbuffer.cls(col = 1)
+        self.Backbuffer.cls(col = 0)
 
         # バックバッファへ描画
-        self.Backbuffer.text(x = 10, y = 10, s = "Hello World", col = 7, font = None)
+        for i in range(0, self.Backbuffer.height // pyxel.FONT_HEIGHT):
+            self.Backbuffer.text(x = self.Backbuffer.width // 2, y = i * pyxel.FONT_HEIGHT, s = "Hello World", col = 7, font = None)
 
-        # バックバッファを画面へ転送
-        pyxel.blt(0, 0, self.Backbuffer, 0, 0, self.Backbuffer.width, self.Backbuffer.height)
+        if self.IsRaster:
+            # バックバッファを画面へ転送 (ラスタースクロール)
+            for i in range(0, self.Backbuffer.height):
+                c = pyxel.cos(deg = (pyxel.frame_count + i) % 360)
+                pyxel.blt(x = c * 50, y = i, img = self.Backbuffer, u = 0, v = i, w = self.Backbuffer.width, h = 1)
+        else:
+            # バックバッファを画面へ転送
+           pyxel.blt(x = 0, y = 0, img = self.Backbuffer, u = 0, v = 0, w = self.Backbuffer.width, h = self.Backbuffer.height)
 
 App()
